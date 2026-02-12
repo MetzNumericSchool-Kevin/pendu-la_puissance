ee2ddc644a3fb87e012d6e01c715051aa3e3ad49
Amina <aminata.fnd@gmail.fr>
2026-02-12 14:58:17 +0100

    feat(pendu): affichage des bonnes lettres dans le placehonlder et du pendu en cas d'erreur + message de victoire ou de défaite

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ function createPlaceholder(zone, classe="i"){
 }
 
 for (let i = 0; i < motChoisi.length; i++){
-    createPlaceholder(zonePlaceholder,'letter-placeholder')
+    //createPlaceholder(zonePlaceholder,'letter-placeholder')
+    const newPlaceholder =  document.createElement('span')
+    zonePlaceholder.appendChild(newPlaceholder)
+    newPlaceholder.classList.add('letter-placeholder')
     console.log(i)      
 }
 
@@ -23,33 +26,56 @@ const lettersUsed = document.querySelector("#letters-used")
 
 const controleLettres = new Set()
 const bonMot = []
-
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let erreur = 0
 addEventListener('keydown', function(e){
     console.log(motChoisi)
-    if (e.key.length == 1){
+    //if (e.key.length == 1 )){
+    if (alphabet.indexOf(e.key) != -1){
         if (controleLettres.has(e.key)){
-            alert("Tu as déjà saisi cette lettre sale con ! 🖕")
+            alert(`Tu as déjà saisi la lettre "${e.key}" ! ☝️`)
             console.log("lettre déjà saisie")
             return
         }
-        createPlaceholder(zoneEssais)
-        document.querySelector(".i:last-child").innerHTML +=`${e.key}`
+        controleLettres.add(e.key)
+        console.log(controleLettres)
+        
+        const newEssai =  document.createElement('span')
+        zoneEssais.appendChild(newEssai)
+    
+        zoneEssais.querySelector("span:last-child").innerHTML +=`${e.key}`
+        if (motChoisi.indexOf(e.key) == -1){
+            erreur += 1
+            document.querySelector(`#hangman>g:nth-child(${erreur+1})`).classList.remove('hidden')
+        }
+        
+        if (erreur >= 5){
+            alert(`Fin du jeu, le bon mot était ${motChoisi} !`)
+            //exit
+        }
+        
         for (let i = 0; i < motChoisi.length; i++){
-            if (e.key == motChoisi[i] ) {
-                
-                document.querySelector(".i:last-child").classList.add("vert")
-                console.log("bonne lettre")
+            if (e.key != motChoisi[i] ) {
+                zoneEssais.querySelector("span:last-child").classList.add("rouge")
+
+            } else {
+              
+                zoneEssais.querySelector("span:last-child").classList.add("vert")
+                //console.log("bonne lettre")
+
                 bonMot.push(e.key)
-            }
-            else {
-                document.querySelector(".i:last-child").classList.add("rouge")
+                document.querySelector(`.letter-placeholder:nth-child(${i+1})`).textContent = e.key
+                //return
             }
         }
         
-        controleLettres.add(e.key)
-        console.log(controleLettres)
+        console.log(`liste avec les bonnes lettres ${bonMot}`)
+        console.log(`longueur du mot choisi ${motChoisi.length}`)
     }
-        
+    
+    if (motChoisi.length === bonMot.length) {
+        alert(`Bravo ! Le bon mot était bien ${motChoisi}`)
+    }  
     })
 //document.querySelector(".letter-placeholder:first-child").textContent = "b"
 
